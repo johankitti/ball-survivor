cb2b8b716ac7b5a85a3b65f22f25a426a30f8efa
johankitti <johan.kitti@gmail.com>
2015-09-08 13:04:10 +0200

    when in air, cannot control balls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,26 +6,31 @@ using System.Collections;
 public class PlayerController : NetworkBehaviour {
 
 	public float speed;
-	public Text countText;
-
+		
+	private Vector3 lastPos;
 	private Rigidbody rb;
 
 	void Start()
 	{
 		rb = GetComponent<Rigidbody>();
-		countText.text = "Hej";
+		lastPos = new Vector3(0f,0.5f,0f);
 	}
 
 	void FixedUpdate()
 	{
 		if (!isLocalPlayer)
 			return;
+		Debug.Log (Mathf.Abs(transform.position.y - lastPos.y));
+		if (Mathf.Abs(transform.position.y - lastPos.y) > 0.1f)
+			return;
+
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
 		Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
 
 		rb.AddForce (movement * speed);
+		lastPos = transform.position;
 	}
 
 	void OnTriggerEnter (Collider other)
