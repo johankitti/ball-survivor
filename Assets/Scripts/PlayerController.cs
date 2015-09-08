3687bb20eb4f9eddaf62c06b449ec802f45d1ad5
johankitti <johan.kitti@gmail.com>
2015-09-08 18:08:47 +0200

    stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,6 +19,8 @@ public class PlayerController : NetworkBehaviour {
 
 	void FixedUpdate()
 	{
+		CheckForReset ();
+
 		if (!isLocalPlayer)
 			return;
 		
@@ -35,6 +37,15 @@ public class PlayerController : NetworkBehaviour {
 			RpcAddClientForce (force);
 		else 
 			CmdAddForce (force);
+	}
+
+	void CheckForReset()
+	{
+		if (transform.position.y < -20f) {
+			rb.velocity = new Vector3(0f,0f,0f);
+			rb.angularVelocity = new Vector3(0f,0f,0f);
+			transform.position = new Vector3 (0f, 3f, 0f);
+		}
 	}
 
 	[Command]
@@ -54,12 +65,24 @@ public class PlayerController : NetworkBehaviour {
 		if (other.gameObject.CompareTag ("Pick Up"))
 		{
 			other.gameObject.SetActive(false);
+			transform.localScale += new Vector3(0.5F, 0.5F, 0.5F);
+			rb.mass = rb.mass * 1.05f;
+			speed = speed * 1.05f;
 		}
 	}
 
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.gameObject.name == "Player(Clone)") 
+		{
+			rb.AddForce (col.rigidbody.velocity * col.rigidbody.mass * 50);
+			col.rigidbody.AddForce (rb.velocity * rb.mass * 50);
+		}
+	}
+	
 	void AssignRandColor () 
 	{
-
+		
 		rend.material.color = new Color(Random.Range (0.0f,1.0f), Random.Range (0.0f,1.0f), Random.Range (0.0f,1.0f));
 	}
 }
